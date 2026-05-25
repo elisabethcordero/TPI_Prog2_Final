420adc22394b3751ee13fc67dea5472dac004f9e
Elisabeth <124397954+elisabethcordero@users.noreply.github.com>
2026-05-25 18:35:51 -0300

    creacion del proyecto

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+
+
+/**
+ * Alumnas: 
+ * Nadia Garcia. 
+ * Analia Franco.
+ * Elisabeth Cordero Campero.
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
